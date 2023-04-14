0fc9e1f5a25e0d014d5cef3bd2fa0213788e73ed
vinigdesouza <vinigdesouza@gmail.com>
2023-04-14 14:19:21 -0300

    finalizado

--- src/database.js
+++ src/database.js
@@ -1,55 +1,68 @@
 import fs from 'node:fs/promises'
 
-const databasePath = new URL('db.json', import.meta.url)
+const databasePath = new URL('../db.json', import.meta.url)
 
-export class Database{
-    #database = {}
+export class Database {
+  #database = {}
 
-    constructor() {
-        fs.readFile(databasePath, 'utf-8').then(data => {
-            this.#database = JSON.parse(data)
-        }).catch(() => {
-            this.#persist()
-        })
-    }
-
-    #persist() {
-        fs.writeFile(databasePath, JSON.stringify(this.#database))
-    }
-
-    select(table) {
-        const data = this.#database[table] ?? []
-
-        return data
-    }
-
-    insert(table, data) {
-        if(Array.isArray(this.#database[table])){
-            this.#database[table].push(data)
-        } else {
-            this.#database[table] = [data]
-        }
-
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
         this.#persist()
+      })
+  }
 
-        return data
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database, null, 2))
+  }
+
+  select(table, search) {
+    let data = this.#database[table] ?? []
+
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          if (!value) return true
+
+          return row[key].includes(value)
+        })
+      })
     }
 
-    delete(table, id) {
-        const rowIndex = this.#database[table].findIndex(row => row.id == id)
+    return data
+  }
 
-        if(rowIndex > -1){
-            this.#database[table].splice(rowIndex, 1)
-            this.#persist()
-        }
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
     }
 
-    update(table, id, data) {
-        const rowIndex = this.#database[table].findIndex(row => row.id == id)
+    this.#persist()
 
-        if(rowIndex > -1){
-            this.#database[table][rowIndex] = {id, ...data}
-            this.#persist()
-        }
+    return data
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      const row = this.#database[table][rowIndex]
+      this.#database[table][rowIndex] = { id, ...row, ...data }
+      this.#persist()
     }
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+  }
 }
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,9 +1,8 @@
-
 export function buildRoutePath(path) {
     const routeParametersRegex = /:([a-zA-Z]+)/g
-    const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
-
-    const pathRegex = new RegExp(`^${pathWithParams}`)
-
-     return pathRegex
-}
+    const paramsWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+  
+    const pathRegex = new RegExp(`^${paramsWithParams}(?<query>\\?(.*))?$`)
+  
+    return pathRegex
+  }
